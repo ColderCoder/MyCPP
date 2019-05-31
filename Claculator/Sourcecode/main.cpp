e411ae73607376e0edc0a20b4204cb764838c74b
ColderCoder <cs9095@live.com>
2019-05-31 19:53:53 +0800

    Update main.cpp

--- Claculator/Sourcecode/main.cpp
+++ Claculator/Sourcecode/main.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
-	char input[] = "1/0";
-	std::cout << calc(input);
+	char input [200];
+	memset(input, 0, sizeof(input));
+	std::cout << "Please input your notation and press Enter:\n";
+	std::cin >> input;
+	std::cout <<std::endl<<"The result is:\n"<< calc(input)<<std::endl;
+	return 0;
 }
